010c1babf514674e4f3c343351de51d2ec249407
Viktoras <pastodezute14@gmail.com>
2023-02-08 16:37:25 +0200

    countries comp with loading, error & data mapping

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,8 +1,29 @@
+import useFetch from '../hooks/useFetch';
+import baseUrl from '../utilities/baseUrl';
+import SingleCountryCard from './SingleCountryCard';
+
 function Countries() {
+  const countriesData = useFetch(baseUrl);
+
   return (
-    <div>
-      <h1>countries comp</h1>
-    </div>
+    <>
+      {countriesData.isLoading && <div>Loading data...</div>}
+
+      {countriesData.error !== undefined ? <div>{countriesData.error}</div> : ''}
+
+      <div>
+        {countriesData.response
+          .map((country) => (
+            <SingleCountryCard
+              key={country.name}
+              name={country.name}
+              area={country.area}
+              flag={country.flag}
+              region={country.region}
+            />
+          ))}
+      </div>
+    </>
   );
 }
 
