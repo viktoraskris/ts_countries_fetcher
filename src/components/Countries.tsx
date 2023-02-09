5cf64cdaef9ad987d8b115052206f7682d672d7c
Viktoras <pastodezute14@gmail.com>
2023-02-09 16:22:23 +0200

    filter forms added to countries comp

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import useFetch from '../hooks/useFetch';
 import baseUrl from '../utilities/baseUrl';
+import handleSortAndFilter from '../utilities/countryUtils';
 import SingleCountryCard from './SingleCountryCard';
+
+enum FilterTypes {
+  BY_REGION = 'FILTER_BY_REGION',
+  BY_LITHUANIA = 'FILTER_COUNTRIES_SMALLER_THAN_LITHUANIA',
+}
+
+enum SortOrder {
+  ASC = 'ASC_ORDER',
+  DESC = 'DESC_ORDER',
+}
 
 function Countries() {
   const countriesData = useFetch(baseUrl);
+  const [countries, setCountries] = useState(countriesData.response);
+
+  const allCountries = [...countriesData.response];
+
+  useEffect(() => {
+    setCountries([...countriesData.response]);
+  }, [countriesData.response]);
 
   return (
     <>
@@ -12,7 +31,29 @@ function Countries() {
       {countriesData.error !== undefined ? <div>{countriesData.error}</div> : ''}
 
       <div>
-        {countriesData.response
+        <div>
+          <form>
+            <label htmlFor="sort">
+              Sorting order:
+              <select onChange={(e) => handleSortAndFilter(countries, setCountries, e.currentTarget.value as SortOrder)} id="sort" defaultValue="sort-order">
+                <option value="sort-order" disabled>Select sorting order:</option>
+                <option value={SortOrder.ASC}>Ascending</option>
+                <option value={SortOrder.DESC}>Descending</option>
+              </select>
+            </label>
+            <label htmlFor="filter">
+              Filter by:
+              <select onChange={(e) => handleSortAndFilter(allCountries, setCountries, e.currentTarget.value as FilterTypes)} id="filter" defaultValue="filter-type">
+                <option value="filter-type" disabled>Select preferred filter</option>
+                <option value="">All countries</option>
+                <option value={FilterTypes.BY_LITHUANIA}>Countries smaller than Lithuania</option>
+                <option value={FilterTypes.BY_REGION}>Oceania region</option>
+              </select>
+            </label>
+          </form>
+        </div>
+
+        {countries
           .map((country) => (
             <SingleCountryCard
               key={country.name}
