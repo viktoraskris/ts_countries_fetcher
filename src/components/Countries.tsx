c2fc9796639b2d1cec0bff018c912424837b752a
Viktoras <pastodezute14@gmail.com>
2023-02-13 10:07:55 +0200

    added basic css

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -25,26 +25,26 @@ function Countries() {
   }, [countriesData.response]);
 
   return (
-    <>
-      {countriesData.isLoading && (<div>Loading data...</div>)}
+    <div>
+      {countriesData.isLoading && (<div className="flex justify-center items-center h-screen text-3xl text-red-600">Loading data...</div>)}
 
-      {countriesData.error !== undefined && (<div>{countriesData.error}</div>)}
+      {countriesData.error !== undefined && (<div className="flex justify-center items-center h-screen text-3xl text-red-600">{countriesData.error}</div>)}
 
       {!countriesData.isLoading && countriesData.error === undefined && (
-      <div>
+      <div className="text-stone-300">
         <div>
-          <form>
+          <form className="flex flex-col m-2 gap-8 sm:flex-row sm:justify-center sm:m-6">
             <label htmlFor="sort">
-              Sorting order:
-              <select onChange={(e) => handleSortAndFilter(countries, setCountries, e.currentTarget.value as SortOrder)} id="sort" defaultValue="sort-order">
+              <span className="text-yellow-500 text-lg font-semibold">Sorting order: </span>
+              <select className="bg-inherit border-b-2" onChange={(e) => handleSortAndFilter(countries, setCountries, e.currentTarget.value as SortOrder)} id="sort" defaultValue="sort-order">
                 <option value="sort-order" disabled>Select sorting order:</option>
                 <option value={SortOrder.ASC}>Ascending</option>
                 <option value={SortOrder.DESC}>Descending</option>
               </select>
             </label>
             <label htmlFor="filter">
-              Filter by:
-              <select onChange={(e) => handleSortAndFilter(allCountries, setCountries, e.currentTarget.value as FilterTypes)} id="filter" defaultValue="filter-type">
+              <span className="text-yellow-500 text-lg font-semibold">Filter by: </span>
+              <select className="bg-inherit border-b-2 " onChange={(e) => handleSortAndFilter(allCountries, setCountries, e.currentTarget.value as FilterTypes)} id="filter" defaultValue="filter-type">
                 <option value="filter-type" disabled>Select preferred filter</option>
                 <option value="">All countries</option>
                 <option value={FilterTypes.BY_LITHUANIA}>Countries smaller than Lithuania</option>
@@ -54,19 +54,23 @@ function Countries() {
           </form>
         </div>
 
-        {countries
-          .map((country) => (
-            <SingleCountryCard
-              key={country.name}
-              name={country.name}
-              area={country.area}
-              flag={country.flag}
-              region={country.region}
-            />
-          ))}
+        <div className="h-screen overflow-auto">
+          <div className="grid grid-cols-1 mx-2 mb-28 sm:grid-cols-2 sm:mb-26 lg:grid-cols-3 xl:grid-cols-4 2xl:w-[1440px] 2xl:mx-auto">
+            {countries
+              .map((country) => (
+                <SingleCountryCard
+                  key={country.name}
+                  name={country.name}
+                  area={country.area}
+                  flag={country.flag}
+                  region={country.region}
+                />
+              ))}
+          </div>
+        </div>
       </div>
       )}
-    </>
+    </div>
   );
 }
 
--- src/components/SingleCountryCard.tsx
+++ src/components/SingleCountryCard.tsx
@@ -10,12 +10,29 @@ function SingleCountryCard(props: CountryProps) {
     area, name, flag, region,
   } = props;
 
+  const areaNum = new Intl.NumberFormat().format(area);
+
   return (
-    <div>
-      <p>{flag}</p>
-      <p>{name}</p>
-      <p>{area}</p>
-      <p>{region}</p>
+    <div className="flex items-center gap-4 py-6 border-b">
+      <div>
+        <img className="w-36" src={flag} alt="Country flag" />
+      </div>
+
+      <div>
+        <p className="font-bold text-xl text-orange-300">{name}</p>
+        <p>
+          Region:
+          {' '}
+          <span className="text-red-500">{region}</span>
+        </p>
+        <p>
+          Area:
+          {' '}
+          <span className="text-green-500">{areaNum === 'NaN' ? 'No data exists' : areaNum}</span>
+          {' '}
+          &#13218;
+        </p>
+      </div>
     </div>
   );
 }
